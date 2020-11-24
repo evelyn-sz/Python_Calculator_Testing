8df93b56d72d8e6f8c85126fa4f1331bb18c7240
evelyn-sz <evelynszarek@gmail.com>
2020-11-24 20:40:08 +0000

    tested all four functions

--- testing_start_code/tests/calculator_test.py
+++ testing_start_code/tests/calculator_test.py
@@ -3,5 +3,13 @@ from src.calculator import add, divide, multiply, subtract
 
 class TestCalculator(unittest.TestCase):
     def test_add(self):
-        self.assertEqual(5, add(2.3))
-        pass
+        self.assertEqual(5, add(2, 3))
+
+    def test_subtract(self):
+        self.assertEqual(3, subtract(10,7))
+
+    def test_divide(self):
+        self.assertEqual(7, divide(63, 9))
+
+    def test_multiply(self):
+        self.assertEqual(72, multiply(8, 9))
